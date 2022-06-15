23b31e519fe6a1684e9224f4707f7684678cc64c
Leanne Afrifa <l.afrifa04@icloud.com>
2022-06-15 15:55:24 +0000

    edits to home + share pages (Leanne)

--- finalProject/uploadViewController.swift
+++ finalProject/uploadViewController.swift
@@ -4,19 +4,31 @@
 //
 //  Created by Anna Hu on 6/14/22.
 //  Copyright © 2022 Anna Hu. All rights reserved.
-//
 
 import UIKit
 
-class uploadViewController: UIViewController {
+class uploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController ()
+    
+    @IBOutlet weak var shareLogo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func selfiePressed(_ sender: Any) {
+        imagePicker.sourceType = .camera
+    }
+    
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
