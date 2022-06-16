1b0130b0921005900259b682dfe48f21cdbf9495
annahu28 <annahuu28@gmail.com>
2022-06-16 00:03:46 -0400

    scroll bar ish and web feature

--- finalProject/homeViewController.swift
+++ finalProject/homeViewController.swift
@@ -11,13 +11,23 @@ import WebKit
 
 class homeViewController: UIViewController {
 
+    
     @IBOutlet weak var ghana: UILabel!
     
+    @IBOutlet weak var ghanaWebView: WKWebView!
+    
+
+    @IBOutlet weak var china: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ghana.text = "This is Ghana!This is Ghana! This is Ghana! This is Ghana! This is Ghana! This is Ghana!This is Ghana! This is Ghana! This is Ghana! This is Ghana! This is Ghana! This is Ghana! This is Ghana! This is Ghana! "
+        ghana.text = "Ghana's new development is on the rise! Click this link to learn more about the affordable housing project. "
+        
+        let ghanaURL = URL(string: "https://www.youtube.com/watch?v=jiuEHckpiKI")
+            ghanaWebView.load(URLRequest(url:ghanaURL!))
+        
+        china.text = "China's new initiative called 'Clear every plate' is set to be implemented next year."
 
         // Do any additional setup after loading the view.
        
