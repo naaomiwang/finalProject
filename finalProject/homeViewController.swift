f5fe94c2c85f64eb1b42d3731ac97e1706db55bf
annahu28 <annahuu28@gmail.com>
2022-06-16 14:46:22 -0400

    home page update

--- finalProject/homeViewController.swift
+++ finalProject/homeViewController.swift
@@ -16,13 +16,17 @@ class homeViewController: UIViewController {
         
     @IBOutlet weak var china: UILabel!
     
+    @IBOutlet weak var singapore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ghana.text = "Ghana's new development is on the rise! Click this link to learn more about the affordable housing project. "
+        ghana.text = "Ghana: Ghana's new development is on the rise! Click this link to learn more about the affordable housing project. "
         
     
-        china.text = "China's new initiative called 'Clear every plate' is set to be implemented next year."
+        china.text = "Read Vogue: The rise and rise of young Chinese designers: Two Chinese designers are semi-finalists for this year’s LVMH Prize. Could this year see a Chinese winner?"
+        
+        singapore.text = "Singapore: The president of Singapore has approved a new infrastructure plan."
 
         // Do any additional setup after loading the view.
        
@@ -30,6 +34,12 @@ class homeViewController: UIViewController {
     @IBAction func ghanaVideoTapped(_ sender: Any) {
         let ghanaURL = URL(string: "https://www.youtube.com/watch?v=jiuEHckpiKI")
         UIApplication.shared.open(ghanaURL!)
+    }
+    
+    
+    @IBAction func chinaTapped(_ sender: Any) {
+        let articleURL = URL(string: "https://www.voguebusiness.com/fashion/the-rise-and-rise-of-young-chinese-designers")
+        UIApplication.shared.open(articleURL!)
     }
     
     /*
