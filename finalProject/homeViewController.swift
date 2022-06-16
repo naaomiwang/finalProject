1609673513b5bc2a2176e7a86d18d4c8ad605a4a
annahu28 <annahuu28@gmail.com>
2022-06-16 11:06:27 -0400

    update

--- finalProject/homeViewController.swift
+++ finalProject/homeViewController.swift
@@ -13,9 +13,7 @@ class homeViewController: UIViewController {
 
     
     @IBOutlet weak var ghana: UILabel!
-    
-    @IBOutlet weak var ghanaWebView: WKWebView!
-    
+        
 
     @IBOutlet weak var china: UILabel!
     
@@ -25,8 +23,7 @@ class homeViewController: UIViewController {
         ghana.text = "Ghana's new development is on the rise! Click this link to learn more about the affordable housing project. "
         
         
-        
-        china.text = "China's new initiative called 'Clear every plate' is set to be implemented next year."
+//        china.text = "China's new initiative called 'Clear every plate' is set to be implemented next year."
 
         // Do any additional setup after loading the view.
        
