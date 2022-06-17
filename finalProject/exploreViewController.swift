c957c625d01fd57f7a387f517aefc24b8d3a93e9
Leanne Afrifa <l.afrifa04@icloud.com>
2022-06-17 00:35:48 +0000

    finished "explore" page, attempted to add app icons

--- finalProject/exploreViewController.swift
+++ finalProject/exploreViewController.swift
@@ -3,18 +3,27 @@
 //  finalProject
 //
 //  Created by Anna Hu on 6/14/22.
-//  Copyright © 2022 Anna Hu. All rights reserved.
+//  Copyright © 2022 Anna Hu + Leanne Afrifa-Akwamoah. All rights reserved.
 //
 
 import UIKit
 
 class exploreViewController: UIViewController {
-
-@IBOutlet weak var label: UILabel!
+var facts = [
+    "All the pandas in the world belong to China, except two.", "Singapore is among the 20 smallest countries in the world.", "Ghana's Lake Volta is the world's biggest man-made lake.", "Ghana is one of the world's leading producers of both gold + cocoa.", "China is the most populated country in the world.", "Singapore's Bukit Timah Nature Reserve has more tree species than the whole North American continent."
+]
+    @IBOutlet weak var searchBar: UISearchBar!
     
-   
-
+    @IBOutlet weak var exploreButtonDisplay: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func exploreButtonTapped(_ sender: UIButton) {
+        let random = Int.random(in:0..<facts.count)
+        exploreButtonDisplay.text = facts[random]
+    }
     }
     
 
